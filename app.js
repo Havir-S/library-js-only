757f0b45a5d8f9f25c6cbd33b700b97b26fd42ce
Chris <saffronkrzysztof@gmail.com>
2019-09-26 10:45:44 +0200

    Initial commit test

---
+++ app.js
@@ -0,0 +1,24 @@
+let myLibrary = [];
+
+function Book(author,year,title,pages,rating,isbn,isRead,img) {
+  this.author = author;
+  this.year = year;
+  this.title = title;
+  this.pages = pages;
+  this.rating = rating;
+  this.isbn = isbn;
+  this.isRead = isRead;
+  this.img = img;
+}
+
+Book.prototype.readingProcess = () => {
+
+}
+
+function addBookToLibrary() {
+
+}
+
+function render() {
+  
+}
