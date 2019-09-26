e1f53420497e863889179a0152c41669518e4735
Chris <saffronkrzysztof@gmail.com>
2019-09-27 01:50:45 +0200

    More details to the book tabs

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ function Book(author,year,title,rating,isbn,isRead,img) {
   this.rating = rating;
   this.isbn = isbn;
   this.isRead = isRead;
-  console.log(arguments);
   this.img = img;
 }
 
@@ -21,9 +20,13 @@ function addBookToLibrary(book) {
   render();
 }
 
+
+// CREATE A BOOK
 let mainColumn = document.getElementsByClassName('main-column')[0];
 
 let createBookTab = (n) => {
+  var book = myLibrary[n];
+  //BASIC MAIN
   var book_tab = document.createElement('div');
   book_tab.className = 'book-tab';
 
@@ -31,13 +34,47 @@ let createBookTab = (n) => {
   book_tab_inside.className = 'book-tab-inside'
   book_tab.appendChild(book_tab_inside);
 
+
+  // DATA-INDEX
   var data_index = document.createAttribute('data-index');
   data_index.value = n;
   book_tab.setAttributeNode(data_index);
 
+  //IMG
+  if(book.img) {
+    var book_image = document.createElement('img');
+    var book_image_src = document.createAttribute('src');
+    book_image_src.value = book.img;
+    var book_image_alt = document.createAttribute('alt');
+    book_image_alt.value = book.img;
+    book_image.setAttributeNode(book_image_src);
+    book_image.setAttributeNode(book_image_alt);
+    book_tab_inside.appendChild(book_image);
+  } else {
+    var book_image = document.createElement('img');
+    var book_image_src = document.createAttribute('src');
+    book_image_src.value = "./books/hobbit.jpg";
+    var book_image_alt = document.createAttribute('alt');
+    book_image_alt.value = 'Error';
+    book_image.setAttributeNode(book_image_src);
+    book_image.setAttributeNode(book_image_alt);
+    book_tab_inside.appendChild(book_image);
+  }
+
+  //BOOK-TAB-INFO
+  var book_tab_info = document.createElement('div');
+  book_tab_info.className = 'book-tab-info';
+  book_tab_info.innerHTML = '<hr><hr>';
+  book_tab_inside.appendChild(book_tab_info);
+
+
+  //DELETE BOOK BUTTON
   var delete_button = document.createElement('div');
   delete_button.innerHTML = 'Click me to delete this';
-  book_tab.appendChild(delete_button);
+  book_tab_inside.appendChild(delete_button);
+
+
+
 
   delete_button.addEventListener('click', function() {
     console.log(n);
@@ -45,9 +82,8 @@ let createBookTab = (n) => {
   });
 
   mainColumn.appendChild(book_tab);
-
-
 }
+
 var bookToDelete;
 let deleteBook = (n) => {
  bookToDelete = document.querySelector('[data-index="'+n+'"]');
@@ -101,13 +137,14 @@ closeModal.addEventListener('click', function() {
 // author,year,title,rating,isbn,isRead,img
 
 let populate = () => {
-  var a = Book('JRR Tolkien','2005','The Fellowship of The Ring','9/10','978-0-00-720354-3',false,'./books/tolkien-tfotr.jpg');
-  var b = Book('Eiji Yoshikawa','2012','Musashi: An Epic Novel of the Samurai Era','9/10','9781568364278',false,'./books/musashi.jpg');
-  var c = Book('Sun Tzu','2015','The Art Of War: Slip-Case Edition','8/10','1784048178',false,'./books/art-of-war.jpg');
-  var d = Book('')
+  var a =new Book('JRR Tolkien','2005','The Fellowship of The Ring','9/10','978-0-00-720354-3',false,'./books/tolkien-tfotr.jpg');
+  var b =new Book('Eiji Yoshikawa','2012','Musashi: An Epic Novel of the Samurai Era','9/10','9781568364278',false,'./books/musashi.jpg');
+  var c =new Book('Sun Tzu','2015','The Art Of War: Slip-Case Edition','8/10','1784048178',false,'./books/art-of-war.jpg');
+  var d =new Book('JRR Tolkien','2012','The Hobbit','8/10','978-0547928227',false,'./books/hobbit.jpg');
   addBookToLibrary(a);
   addBookToLibrary(b);
   addBookToLibrary(c);
+  addBookToLibrary(d);
 }
 
 
